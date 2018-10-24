4fa22604d4f62ca1d967bb4961609b3e1fb8276d
Jim Tittsler <jimt@onjapan.net>
2018-10-24 20:45:21 +0900

    Put -_ in base layer

--- keyboards/atreus/keymaps/jimt/keymap.c
+++ keyboards/atreus/keymaps/jimt/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,      KC_W,       KC_E,     KC_R,          KC_T,                                            KC_Y,             KC_U,     KC_I,     KC_O,          KC_P,
     KC_A,      KC_S,       KC_D,     KC_F,          KC_G,                                            KC_H,             KC_J,     KC_K,     KC_L,          KC_SCLN,
     KC_Z,      KC_X,       KC_C,     KC_V,          KC_B,                                            KC_N,             KC_M,     KC_COMM,  KC_DOT,        KC_SLSH,
-    CTL_T(KC_ESC), KC_TAB, KC_LGUI,  SFT_T(KC_DEL), LT(LOWR, KC_BSPC), SFT_T(KC_ESC), CTL_T(KC_ENT), LT(RAIS, KC_SPC), KC_EQL,   KC_BSLS,  KC_QUOT,       ALT_T(KC_GRV)
+    CTL_T(KC_ESC), KC_TAB, KC_LGUI,  SFT_T(KC_DEL), LT(LOWR, KC_BSPC), SFT_T(KC_ESC), CTL_T(KC_ENT), LT(RAIS, KC_SPC), KC_MINUS, KC_BSLS,  KC_QUOT,       ALT_T(KC_GRV)
   ),
   [LOWR] = LAYOUT(
     KC_F1,     KC_F2,      KC_F3,    KC_F4,         KC_F5,                                           KC_F6,            KC_F7,    KC_F8,    KC_F9,         KC_F10,
